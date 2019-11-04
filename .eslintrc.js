78b4b20a025cfbcb8a12d0059142ae326dc0ded0
Advait Junnarkar <advaitju@users.noreply.github.com>
2019-11-04 13:35:39 +1100

    Update eslintrc with React rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
   extends: ['airbnb-base', 'prettier'],
   plugins: ['prettier'],
+  rules: {
+    indent: [2, 'tab'],
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js', '.jsx'],
+      },
+      'react/jsx-indent': [2, 'tab'],
+  },
 };
