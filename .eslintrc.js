aa0af31993cf22fc792b69e8fd2647ad8eff67b0
Advait Junnarkar <advaitju@users.noreply.github.com>
2019-11-05 11:24:15 +1100

    Update eslintrc with Prettier rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,17 @@
 module.exports = {
-  extends: ['airbnb-base', 'prettier'],
-  plugins: ['prettier'],
-  rules: {
-    indent: [2, 'tab'],
-    'react/jsx-filename-extension': [
-      1,
-      {
-        extensions: ['.js', '.jsx'],
-      },
-      'react/jsx-indent': [2, 'tab'],
-  },
+	extends: ['airbnb', 'prettier', 'prettier/react'],
+	plugins: ['prettier'],
+	rules: {
+		'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+		'prettier/prettier': [
+			'error',
+			{
+				endOfLine: 'lf',
+				singleQuote: true,
+				useTabs: true,
+				tabWidth: 2,
+				trailingComma: 'es5',
+			},
+		],
+	},
 };
