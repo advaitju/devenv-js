cb10352760b1c7ec20f88ca5336a8e70ee07db71
Advait Junnarkar <advaitju@users.noreply.github.com>
2020-01-19 10:56:11 +1100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,16 @@
-#! /bin/bash
+#!
 
-echo "--- SETUP STARTED ---"
+if [[ $1 == --n || $1 == -npm ]]; then
+	echo "Selected packagemanager: npm"
+elif [[ $1 == --p || $1 == -pnpm ]]; then
+	echo "Selected packagemanager: pnpm"
+else
+	echo "Error: Specify whether to install with NPM (-p --npm) or PNPM (-p --pnpm)."
+	exit 1
+fi
+
+echo "--- SETUP STARTED ---" /bin/bash
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Delete .git files and don't include README in devnenv setup
@@ -17,8 +27,6 @@ if [[ $1 == --n || $1 == -npm ]]; then
 	npx install-peerdeps -d --prefix $DIR/.. eslint-config-airbnb
 elif [[ $1 == --p || $1 == -pnpm ]]; then
 	pnpm i -D --prefix $DIR/.. eslint eslint-config-airbnb eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react- eslint-plugin-react-hooks eslint-config-prettier eslint-plugin-prettier prettier
-else
-	echo "Error: Specify whether to install with NPM (-p --npm) or PNPM (-p --pnpm)."
 fi
 
 # Delete setup script
