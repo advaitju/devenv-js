cc4bb7f816a74dc956d4a7dde51a8bfac0932b4f
Advait Junnarkar <advaitju@users.noreply.github.com>
2021-03-17 18:00:53 +1100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -18,7 +18,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo "Directory name: $DIR"
 
 IFS='/' read -ra ADDR <<< "$DIR"
-PWD_NAME=${ADDR[-1]}
+PWD_NAME=${ADDR[-2]}
 echo "Folder name: $PWD_NAME"
 
 # Delete .git files and don't include README in devnenv setup
